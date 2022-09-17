7b36dc9ae8f820932363a7ad847d6dd963ac76bd
technoya <113179663+technoya@users.noreply.github.com>
2022-09-17 01:17:14 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,9 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <a href="https://about.phamvanlam.com" target="_blank" rel="noreferrer noopener">
-          Home
-        </a>
+       
         {" "}
-        <a href="https://github.com/completejavascript/tic-tac-toe" target="_blank" rel="noreferrer noopener">
-          Github
+       
         </a>
       </footer>
     );
